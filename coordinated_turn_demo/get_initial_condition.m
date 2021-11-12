986a10f40e201bbfd7d8d8ac9af921d9d7c200b9
ningyuanwang <wang.ningyuan.mail2@gmail.com>
2021-11-12 11:10:53 +0900

    Fixed the input of get_initial_condition.m to take variable input.

--- coordinated_turn_demo/get_initial_condition.m
+++ coordinated_turn_demo/get_initial_condition.m
@@ -1,6 +1,10 @@
-function [xS_initial] = get_initial_condition(w0_degree,w0_sigma_degree=5.72958)
+function [xS_initial] = get_initial_condition(w0_degree,varargin)
 %Generates initial condition used for the coordinated turns model
 %   w0: rotation speed in degrees per second
+w0_sigma_degree=5.72958;
+if nargin >= 2
+    w0_sigma_degree = varargin{1};
+end
 x0_initial = [1000;0;2650;150;200;0;w0_degree*pi/180];
 Sigma0 = diag([1e1;1;1e1;1;1e1;1;w0_sigma_degree*pi/180].^2);
 U0 = chol(Sigma0);
